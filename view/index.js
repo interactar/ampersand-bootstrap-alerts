9e58ce7348f01aafdb17e6fb6792d268caa10108
Facundo Gonzalez <facugon@interactar.com>
2016-08-17 11:01:22 -0300

    remove element on hide or animation completed

--- view/index.js
+++ view/index.js
@@ -29,14 +29,24 @@ module.exports = View.extend({
     typeClass:{type:'attribute',name:'class',hook:'alert-type'}
   },
   render:function(){
+    var self = this;
     this.renderWithTemplate(this);
-    $('body .alerts-container').append( this.el );
+    $('body .alerts-container').html( this.el );
 
-    var $alert = $('.alert');
+    var $alert = this.$alert = $('.alert');
+    $alert.on('closed.bs.alert',function(){ self.remove(); });
     $alert.slideDown();
 
     window.setTimeout(function(){
-      $alert.slideUp();
+      $alert.slideUp({
+        complete:function(){
+          $alert.trigger('closed.bs.alert');
+        }
+      });
     },this.timeout * 1000);
+  },
+  remove:function(){
+    this.$alert.remove();
+    View.prototype.remove.call(this);
   }
 });
